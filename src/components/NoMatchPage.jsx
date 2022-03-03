02674102304a6f17c07653d36083252ce9ef2135
evdokimoww <La15mCrSP1>
2022-03-03 13:14:04 +0300

    404 page

--- src/components/NoMatchPage.jsx
+++ src/components/NoMatchPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const NoMatchPage = () => {
   return (
-    <h1>Нет страницы лучше в мире чем 404</h1>
+    <Row className={'row justify-content-center align-content-center h-100'}>
+      <Col>
+        <div className={'text-center align-content-center'}>
+          <h3>Нет страницы лучше в мире чем 404...</h3>
+          <p className={'text-muted'}><a href="/">на главную</a></p>
+        </div>
+      </Col>
+    </Row>
+
   )
 };
 
